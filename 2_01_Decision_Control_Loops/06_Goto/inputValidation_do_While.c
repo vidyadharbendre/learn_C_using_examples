add9cb11a96203abbb00baad2bbcc5f93c1e0c43
Vidyadhar Bendre <vidyadharbendre@Vidyadhars-MacBook-Pro.local>
2024-11-21 10:08:18 +0530

    modified do while

--- 2_01_Decision_Control_Loops/06_Goto/inputValidation_do_While.c
+++ 2_01_Decision_Control_Loops/06_Goto/inputValidation_do_While.c
@@ -1,18 +1,19 @@
 /*
-    Program Name: InputValidationWithDoWhile.c
+    Program Name: InputValidationWithFlag.c
     Author: Vidyadhar Bendre
     Email: vidyadhar.bendre@gmail.com
 
     Description:
-    This program demonstrates input validation using a `do-while` loop.
-    The program prompts the user to enter a number within a valid range
-    and continues prompting until the input is valid.
+    This program demonstrates input validation using a `do-while` loop with a flag variable.
+    The program prompts the user to enter a number within a valid range and continues 
+    prompting until the input is valid.
 */
 
 #include <stdio.h>
 
 int main() {
     int number;
+    int flag = 1; // Flag to control the loop
 
     // Prompt the user and validate input using a do-while loop
     do {
@@ -20,10 +21,12 @@ int main() {
         scanf("%d", &number);
 
         // Check if the input is valid
-        if (number < 1 || number > 10) {
+        if (number >= 1 && number <= 10) {
+            flag = 0; // Set flag to 0 to exit the loop
+        } else {
             printf("Invalid input! Please try again.\n");
         }
-    } while (number < 1 || number > 10); // Repeat until the input is valid
+    } while (flag); // Loop continues as long as flag is 1
 
     // If input is valid
     printf("Thank you! You entered a valid number: %d\n", number);
@@ -34,11 +37,12 @@ int main() {
 /*
     Example Output:
 
+Enter a number between 1 and 10: -7
+Invalid input! Please try again.
 Enter a number between 1 and 10: -6
 Invalid input! Please try again.
 Enter a number between 1 and 10: 0
 Invalid input! Please try again.
-Enter a number between 1 and 10: 5
-Thank you! You entered a valid number: 5
-
+Enter a number between 1 and 10: 3
+Thank you! You entered a valid number: 3
 */
